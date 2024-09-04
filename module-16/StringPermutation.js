d02cfeb1d1560f7e7fd202ac9c30c6cb3c67bc21
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-09-04 20:21:59 +0600

    Update StringPermutation.js

--- module-16/StringPermutation.js
+++ module-16/StringPermutation.js
@@ -9,3 +9,46 @@ Input: abc
 Output:abc acb bac bca cab cba
 Notes: None
  */
+
+function main(input) {
+	/**
+	 * Write JavaScript code from here
+	 */
+	var arr = [];
+	function permute(str, l, r) {
+		if (l == r) {
+			arr.push(str);
+			//console.log(str+ " ");
+		} else {
+			for (let i = l; i <= r; i++) {
+				str = swap(str, l, i);
+				permute(str, l + 1, r);
+				str = swap(str, l, i);
+			}
+		}
+	}
+
+	function swap(a, i, j) {
+		let temp;
+		let charArray = a.split("");
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return charArray.join("");
+	}
+
+	let str = input;
+	let n = str.length;
+	permute(str, 0, n - 1);
+
+	console.log(...arr.sort());
+}
+
+let input = "";
+process.stdin.on("data", (chunk) => {
+	input += chunk;
+});
+
+process.stdin.on("end", () => {
+	main(input.trim());
+});
