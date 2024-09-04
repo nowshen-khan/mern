596ef1d9094f7c26ed88857e5cc1ba0cac0da09d
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-09-04 20:11:30 +0600

    Update LongestSubstringWithoutRepeatingCharacter.js

--- module-16/LongestSubstringWithoutRepeatingCharacter.js
+++ module-16/LongestSubstringWithoutRepeatingCharacter.js
@@ -10,3 +10,44 @@ Input: abcabcbb
 Output: 3
 Notes: None
  */
+
+function main(input) {
+	/**
+	 * Write JavaScript code from here
+	 */
+
+	function longestUniqueSubstr(s) {
+		let n = s.length;
+		let res = 0;
+
+		for (let i = 0; i < n; i++) {
+			// Initializing all characters as not visited
+			let visited = new Array(256).fill(false);
+
+			for (let j = i; j < n; j++) {
+				// If current character is visited
+				// Break the loop
+				if (visited[s.charCodeAt(j)] === true) {
+					break;
+				} else {
+					// Else update the result if this window is larger,
+					// and mark current character as visited.
+					res = Math.max(res, j - i + 1);
+					visited[s.charCodeAt(j)] = true;
+				}
+			}
+		}
+		return res;
+	}
+
+	console.log(longestUniqueSubstr(input));
+}
+
+let input = "";
+process.stdin.on("data", (chunk) => {
+	input += chunk;
+});
+
+process.stdin.on("end", () => {
+	main(input.trim());
+});
